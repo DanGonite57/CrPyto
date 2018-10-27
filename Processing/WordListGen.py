a77d09024744f0571cd9dcad2c4ecb3923be2e7f
Daniel Zhang <danzinc57@gmail.com>
2018-10-27 18:56:25 +0100

    Updated

--- Processing/WordListGen.py
+++ Processing/WordListGen.py
@@ -1,40 +1,33 @@
+import PatternGen
+
 with open("rawWords.txt") as f:
     words = f.read()
 
-print(0)
 words = words.split()
-print(1)
 f = open("WordList.txt", "a")
-print(2)
 
 tmp = set()
-print(3)
 for word in words:
-    if word.isalnum():
+    if word.isalnum() and len(word) > 1:
         tmp.add(word.lower())
-print(4)
 tmp = sorted(tmp)
-print(5)
 for x in tmp:
-    f.write(x+"\n")
+    f.write(x + "\n")
 f.close()
 
+patterns = {}
 
-# import PatternGen
+with open("WordList.txt") as f:
+    words = f.readlines()
 
-# patterns = {}
+for word in words:
+    word = word[:-1]
+    try:
+        patterns[PatternGen.pattern(word)].append(word)
+    except KeyError:
+        patterns[PatternGen.pattern(word)] = []
+        patterns[PatternGen.pattern(word)].append(word)
 
-# with open("D:\Programming\RandPy\Codebreaking No. 3\Ciphers\WordList.txt") as f:
-#    words = f.readlines()
-
-# for word in words:
-#    word = word[:-1]
-#    try:
-#        patterns[PatternGen.pattern(word)].append(word)
-#    except KeyError:
-#        patterns[PatternGen.pattern(word)] = []
-#        patterns[PatternGen.pattern(word)].append(word)
-
-# f = open("D:\Programming\RandPy\Codebreaking No. 3\Ciphers\PatternList.py", "w")
-# f.write(str(patterns))
-# f.close()
+f = open("PatternList.py", "w")
+f.write(str(patterns))
+f.close()
