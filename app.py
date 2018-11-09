d5744d92c51f4a75d7ff7f6512268e98569d7758
Daniel Zhang <danzinc57@gmail.com>
2018-11-09 18:09:03 +0000

    QOL

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
-from views.ciphers import ciphers
+from views import ciphers, tools
 import routes
 
-app.register_blueprint(ciphers)
+app.register_blueprint(ciphers.ciphers)
+app.register_blueprint(tools.tools)
---
+++ views/tools.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+tools = Blueprint("tools", __name__, url_prefix="/tools")
