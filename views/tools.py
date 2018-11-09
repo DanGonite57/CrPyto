35e5f1840b413d55dc078d8d2bd280cd15134585
Daniel Zhang <danzinc57@gmail.com>
2018-11-09 20:12:38 +0000

    Init

--- views/tools.py
+++ views/tools.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 tools = Blueprint("tools", __name__, url_prefix="/tools")
+
+
+@tools.route("/addspaces.html")
+def addSpaces():
+    return render_template("tools/addspaces.html")
+
+
+@tools.route("/removespaces.html")
+def removeSpaces():
+    return render_template("tools/removespaces.html")
+
+
+@tools.route("/removepunctuation.html")
+def removePunctuation():
+    return render_template("tools/removepunctuation.html")
