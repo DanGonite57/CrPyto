458cc3ce47e8add1b100880473e23559606ae7f5
Daniel Zhang <danzinc57@gmail.com>
2018-11-10 10:09:33 +0000

    Added template

--- views/tools.py
+++ views/tools.py
@@ -5,14 +5,14 @@ tools = Blueprint("tools", __name__, url_prefix="/tools")
 
 @tools.route("/addspaces.html")
 def addSpaces():
-    return render_template("tools/addspaces.html")
+    return render_template("tools/addspaces.html", title="Add Spaces")
 
 
 @tools.route("/removespaces.html")
 def removeSpaces():
-    return render_template("tools/removespaces.html")
+    return render_template("tools/removespaces.html", title="Remove Spaces")
 
 
 @tools.route("/removepunctuation.html")
 def removePunctuation():
-    return render_template("tools/removepunctuation.html")
+    return render_template("tools/removepunctuation.html", title="Remove Punctuation")
