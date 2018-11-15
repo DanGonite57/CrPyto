7e3516b3e4f976f94e0acc03f461444173612685
Daniel Zhang <danzinc57@gmail.com>
2018-11-15 20:23:42 +0000

    Added functionality

--- views/tools.py
+++ views/tools.py
@@ -1,24 +1,39 @@
 from flask import Blueprint, render_template, request
 
-from Formatting import SpaceAdd
+from Formatting import PuncRem, SpaceAdd, SpaceRem
 from Processing import DetectEnglish
 
 tools = Blueprint("tools", __name__, url_prefix="/tools")
 
 
-@tools.route("/addspaces.html")
+@tools.route("/addspaces.html", methods=["GET", "POST"])
 def addSpaces():
-    return render_template("tools/addspaces.html", title="Add Spaces")
+    args = {"title": "Add Spaces", "ciphText": "", "result": "", "score": 0, "vals": {}, "keylen": ""}
+    if request.method == "POST":
+        args["ciphText"] = ciph = request.form["ciphInput"]
+        args["result"] = plain = SpaceAdd.add(ciph)
+        args["score"] = DetectEnglish.detectWord(plain) * 100
+    return render_template(f"tools/addspaces.html", **args)
 
 
-@tools.route("/removespaces.html")
+@tools.route("/removespaces.html", methods=["GET", "POST"])
 def removeSpaces():
-    return render_template("tools/removespaces.html", title="Remove Spaces")
+    args = {"title": "Remove Spaces", "ciphText": "", "result": "", "score": 0, "vals": {}, "keylen": ""}
+    if request.method == "POST":
+        args["ciphText"] = ciph = request.form["ciphInput"]
+        args["result"] = plain = SpaceRem.remove(ciph)
+        args["score"] = DetectEnglish.detectWord(SpaceAdd.add(plain)) * 100
+    return render_template(f"tools/removespaces.html", **args)
 
 
-@tools.route("/removepunctuation.html")
+@tools.route("/removepunctuation.html", methods=["GET", "POST"])
 def removePunctuation():
-    return render_template("tools/removepunctuation.html", title="Remove Punctuation")
+    args = {"title": "Remove Punctuation", "ciphText": "", "result": "", "score": 0, "vals": {}, "keylen": ""}
+    if request.method == "POST":
+        args["ciphText"] = ciph = request.form["ciphInput"]
+        args["result"] = plain = PuncRem.remove(ciph)
+        args["score"] = DetectEnglish.detectWord(SpaceAdd.add(plain)) * 100
+    return render_template(f"tools/removepunctuation.html", **args)
 
 
 @tools.route("/reversetext.html", methods=["GET", "POST"])
