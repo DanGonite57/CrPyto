444e619794cc38349a60ca9606043fb3dde0699f
Daniel Zhang <danzinc57@gmail.com>
2018-11-28 09:02:38 +0000

    Added variable errors

--- routes.py
+++ routes.py
@@ -21,27 +21,11 @@ def index():
     return render_template('index.html', title="CrPyto")
 
 
-@app.errorhandler(403)
-def e403(error):
-    # return render_template(f"errors/{str(e)}.html"), e
-    return render_template("errors/403.html", title="403", error=error), 403
-
-
-@app.errorhandler(404)
-def e404(error):
-    return render_template("errors/404.html", title="404", error=error), 404
-
-
-@app.errorhandler(410)
-def e410(error):
-    return render_template("errors/410.html", title="410", error=error), 410
-
-
-@app.errorhandler(500)
-def e500(error):
-    return render_template("errors/500.html", title="500", error=error), 500
+@app.errorhandler(e)
+def error(e):
+    return render_template(f"errors/{e}.html", title=e), e
 
 
 @app.errorhandler(Exception)
 def oof(error):
-    return render_template("errors/500.html", title="500", error=error), 500
+    return render_template("errors/500.html", title="500"), 500
