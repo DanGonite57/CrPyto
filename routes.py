45ce8f26b77bb31d54e7e9e7f883102877bd6192
Daniel Zhang <danzinc57@gmail.com>
2018-12-06 18:46:16 +0000

    Added variable error switch

--- routes.py
+++ routes.py
@@ -19,17 +19,3 @@ def root():
 @app.route("/index.html")
 def index():
     return render_template('index.html', title="CrPyto")
-
-
-# @app.errorhandler(403)
-# @app.errorhandler(404)
-# @app.errorhandler(410)
-# @app.errorhandler(500)
-@app.errorhandler(<e>)
-def error(e):
-    return render_template(f"errors/{e}.html", title=e), e
-
-
-@app.errorhandler(Exception)
-def oof(error):
-    return render_template("errors/500.html", title="500"), 500
---
+++ views/errors.py
@@ -0,0 +1,22 @@
+from flask import Markup, render_template, request
+from werkzeug.exceptions import HTTPException, default_exceptions
+
+
+def error_handler(error):
+    if isinstance(error, HTTPException):
+        msg = Markup(error.get_description(request.environ))
+        code = error.code
+        name = error.name
+    else:
+        msg = ("We encountered an error while trying to fulfill your request")
+        code = 500
+        name = 'Internal Server Error'
+
+    return render_template([f"errors/{code}.html", "errors/500.html"], title=f"{code} - {name}", code=code, msg=msg)
+
+
+def init(app):
+    for exception in default_exceptions:
+        app.register_error_handler(exception, error_handler)
+
+    app.register_error_handler(Exception, error_handler)
