492eaf2f4581d767b9b765ed99ae974328086330
Daniel Zhang <danzinc57@gmail.com>
2019-01-09 09:21:14 +0000

    Readd deleted SpaceAdd

---
+++ Formatting/SpaceAdd.py
@@ -0,0 +1,51 @@
+from string import whitespace as SPACE
+
+from Formatting import Format
+from Processing import DetectEnglish
+
+
+def __process(text, maxWord, words, poss):
+    text = Format.remove(text, SPACE).lower()
+    textLen = len(text)
+    maxLen = 0
+    bestWord = ""
+    word = ""
+    for x in range(textLen):
+        for y in range(x + maxWord, x, -1):
+            if "." not in text[x:y]:
+                word = text[x:y]
+                if DetectEnglish.detectWord(word) == 1:
+                    if len(word) > maxLen:
+                        maxLen = len(word)
+                        bestWord = str(word)
+                    break
+
+    words[text.index(bestWord)] += bestWord
+    text = (
+        text[: text.index(bestWord)]
+        + "." * len(bestWord)
+        + text[text.index(bestWord) + len(bestWord)::]
+    )
+
+    if maxLen == 0:
+        words[text.index(word)] += word
+        text = (
+            text[: text.index(word)]
+            + "." * len(word)
+            + text[text.index(word) + len(word)::]
+        )
+        maxLen = 1
+
+    if set(text) == {"."}:
+        st = " ".join(words)
+        return " ".join(st.split())
+
+    __process(text, maxLen, words, poss)
+
+    st = " ".join(words)
+    return " ".join(st.split())
+
+
+def add(text):
+    text += "."
+    return __process(text, DetectEnglish.getLongest(), [""] * (len(text)), [])
