bf617596b782bdef82952d33f6f7fb12fe109353
Daniel Zhang <danzinc57@gmail.com>
2019-01-30 09:44:17 +0000

    Reduced WordList

--- Processing/WordListGen.py
+++ Processing/WordListGen.py
@@ -1,23 +1,23 @@
 import PatternGen
 
-with open("rawWords.txt") as f:
-    words = f.read()
+# with open("rawWords.txt") as f:
+#     words = f.read()
 
-words = words.split()
-f = open("WordList.txt", "a")
+# words = words.split()
+# f = open("WordList.txt", "a")
 
-tmp = set()
-for word in words:
-    if word.isalnum() and len(word) > 1:
-        tmp.add(word.lower())
-tmp = sorted(tmp)
-for x in tmp:
-    f.write(x + "\n")
-f.close()
+# tmp = set()
+# for word in words:
+#     if word.isalnum() and len(word) > 1:
+#         tmp.add(word.lower())
+# tmp = sorted(tmp)
+# for x in tmp:
+#     f.write(x + "\n")
+# f.close()
 
 patterns = {}
 
-with open("WordList.txt") as f:
+with open("static/WordList.txt") as f:
     words = f.readlines()
 
 for word in words:
@@ -28,6 +28,6 @@ for word in words:
         patterns[PatternGen.pattern(word)] = []
         patterns[PatternGen.pattern(word)].append(word)
 
-f = open("PatternList.py", "w")
+f = open("processing/PatternList.py", "w")
 f.write(str(patterns))
 f.close()
