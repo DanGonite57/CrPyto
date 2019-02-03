c5dc08e74397069a1b94ead759f35c663811874b
Daniel Zhang <danzinc57@gmail.com>
2019-02-03 16:50:31 +0000

    Cleaned up

--- Formatting/Format.py
+++ Formatting/Format.py
@@ -2,18 +2,14 @@ from string import ascii_letters as ALPH
 
 
 def remove(text, *args):
-    chars = ""
-    for x in args:
-        chars += x
+    chars = ''.join(args)
     for char in chars:
         text = text.replace(char, "")
     return text
 
 
 def keepOnly(text, *args):
-    chars = ""
-    for x in args:
-        chars += x
+    chars = ''.join(args)
     new = ""
     for char in text:
         if char in chars:
