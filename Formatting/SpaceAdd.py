6905d41de0f1640cd4ca394146ff44bf7d9f49e8
Daniel Zhang <danzinc57@gmail.com>
2021-06-27 19:10:12 +0100

    feat: add spaces to text

--- Formatting/SpaceAdd.py
+++ Formatting/SpaceAdd.py
@@ -7,12 +7,39 @@ Formatting.SpaceAdd
 This module automatically inserts the optimal spacing in a block of text.
 """
 
-from wordsegment import load, segment
+from string import ascii_lowercase as ALPH
 
-load()
+from Formatting import Format
+
+from Processing import DetectEnglish
 
 
-def add(text):
+def addForwards(text):
     """Insert spacing into text."""
 
-    return ' '.join(segment(text))
+    with open("static/txt/wordlist.txt", encoding="utf-8") as f:
+        wordset = f.read().split("\n")
+
+    string = Format.keepOnly(text, ALPH)
+
+    result = []
+    maxLen = DetectEnglish.getLongest()
+
+    x = maxLen
+    while True:
+        word = string[:x]
+        if word in wordset:
+            result.append(word)
+            string = string[x::]
+            x = maxLen
+        else:
+            x -= 1
+
+        if x == 0 and string:
+            result.append(string[0])
+            string = string[1::]
+            x = maxLen
+        elif not string:
+            break
+
+    return " ".join(result)
